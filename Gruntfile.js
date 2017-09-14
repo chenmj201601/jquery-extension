a6bf233f44b1670ce60caa2ce570291e640797bd
chenmj201601 <609539125@qq.com>
2017-09-14 14:12:05 +0800

    拆分 src 源文件

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,27 @@
-module.exports=function(grunt){
-	grunt.initConfig({
-		pkg:grunt.file.readJSON('package.json'),
-		concat:{
-			options:{
-				separator:';'
-			},
-			dist:{
-				src:['src/jquery-extension.js'],
-				dest:'dest/jquery-extension.js'
-			}
-		},
-		uglify:{
-			options:{
-				banner:'/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-			},
-			build:{
-				src:'src/jquery-extension.js',
-				dest:'dest/jquery-extension.min.js'
-			}
-		}
-	});
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('default',['concat','uglify']);
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';\n',
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            dist: {
+                src: ['src/BASE64.js', 'src/DateHelper.js'],
+                dest: 'dest/jquery-extension.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'dest/jquery-extension.js',
+                dest: 'dest/jquery-extension.min.js'
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['concat', 'uglify']);
 };
